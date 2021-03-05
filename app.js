05dcb3e0b8a495b75bc265500ca10189778b3b62
Kuvonchbek Otaboev <00009185@wiut.uz>
2021-03-05 16:53:54 +0500

    start from

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+
+})
+
+app.listen(8000, err => {
+	if (err) throw err
+
+	console.log('App is running...')
+})

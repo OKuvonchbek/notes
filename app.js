32289d951ce14c7348d442b5dc0e7231716f35d5
Kuvonchbek Otaboev <00009185@wiut.uz>
2021-03-12 17:57:39 +0500

    add some new pages

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+const notes = [
+    'Some awesome notes1',
+    'Some awesome notes2',
+    'Some awesome notes3'
+];
+
+
 app.use('/static', express.static('public'))
 
 app.set('view engine', 'pug')
@@ -8,7 +15,15 @@ app.set('view engine', 'pug')
 app.get('/', (req, res) => {
 	res.render('home')
 })
-
+app.get('/create', (req, res) => {
+	res.render('create')
+})
+app.get('/notes', (req, res) => {
+	res.render('notes', { n: notes })
+})
+app.get('/notes/1', (req, res) => {
+	res.render('detail')
+})
 app.listen(8000, err => {
 	if (err) throw err
 
